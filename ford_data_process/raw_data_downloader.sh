6f7582a716aaf3a65eff9324e53ca965e57dd332
Shan <shan.wang@anu.edu.au>
2024-05-03 20:00:06 +1000

    Update raw_data_downloader.sh
    only use log4

--- ford_data_process/raw_data_downloader.sh
+++ ford_data_process/raw_data_downloader.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-files=(2017-10-26/V2/Log3/2017-10-26-V2-Log3-FL.tar.gz
-2017-10-26/V2/Log3/2017-10-26-V2-Log3-RR.tar.gz
-2017-10-26/V2/Log3/2017-10-26-V2-Log3-SL.tar.gz
-2017-10-26/V2/Log3/2017-10-26-V2-Log3-SR.tar.gz
-2017-10-26/V2/Log4/2017-10-26-V2-Log4-FL.tar.gz
+files=(2017-10-26/V2/Log4/2017-10-26-V2-Log4-FL.tar.gz
 2017-10-26/V2/Log4/2017-10-26-V2-Log4-RR.tar.gz
 2017-10-26/V2/Log4/2017-10-26-V2-Log4-SL.tar.gz
 2017-10-26/V2/Log4/2017-10-26-V2-Log4-SR.tar.gz
-2017-10-26/V2/Log5/2017-10-26-V2-Log5-FL.tar.gz
-2017-10-26/V2/Log5/2017-10-26-V2-Log5-RR.tar.gz
-2017-10-26/V2/Log5/2017-10-26-V2-Log5-SL.tar.gz
-2017-10-26/V2/Log5/2017-10-26-V2-Log5-SR.tar.gz
-2017-10-26/V2/Log6/2017-10-26-V2-Log6-FL.tar.gz
-2017-10-26/V2/Log6/2017-10-26-V2-Log6-RR.tar.gz
-2017-10-26/V2/Log6/2017-10-26-V2-Log6-SL.tar.gz
-2017-10-26/V2/Log6/2017-10-26-V2-Log6-SR.tar.gz
-2017-08-04/V2/Log3/2017-08-04-V2-Log3-FL.tar.gz
-2017-08-04/V2/Log3/2017-08-04-V2-Log3-RR.tar.gz
-2017-08-04/V2/Log3/2017-08-04-V2-Log3-SL.tar.gz
-2017-08-04/V2/Log3/2017-08-04-V2-Log3-SR.tar.gz
 2017-08-04/V2/Log4/2017-08-04-V2-Log4-FL.tar.gz
 2017-08-04/V2/Log4/2017-08-04-V2-Log4-RR.tar.gz
 2017-08-04/V2/Log4/2017-08-04-V2-Log4-SL.tar.gz
-2017-08-04/V2/Log4/2017-08-04-V2-Log4-SR.tar.gz
-2017-08-04/V2/Log5/2017-08-04-V2-Log5-FL.tar.gz
-2017-08-04/V2/Log5/2017-08-04-V2-Log5-RR.tar.gz
-2017-08-04/V2/Log5/2017-08-04-V2-Log5-SL.tar.gz
-2017-08-04/V2/Log5/2017-08-04-V2-Log5-SR.tar.gz
-2017-08-04/V2/Log6/2017-08-04-V2-Log6-FL.tar.gz
-2017-08-04/V2/Log6/2017-08-04-V2-Log6-RR.tar.gz
-2017-08-04/V2/Log6/2017-08-04-V2-Log6-SL.tar.gz
-2017-08-04/V2/Log6/2017-08-04-V2-Log6-SR.tar.gz)
+2017-08-04/V2/Log4/2017-08-04-V2-Log4-SR.tar.gz)
 
 
 wget 'https://ford-multi-av-seasonal.s3-us-west-2.amazonaws.com/Calibration/Calibration-V2.tar.gz'
